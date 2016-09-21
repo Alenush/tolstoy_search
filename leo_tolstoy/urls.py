ed7fbc24e89dc7e8b9b4affd00ae40f7185303fd
alenush <alenusch93@yandex.ru>
2016-09-22 01:39:04 +0300

    Made fron pages and first styles

---
+++ leo_tolstoy/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'data$', views.show_data, name='show_data'),
+    url(r'search$', views.start_search, name='search_main'),
+]
--- leo_tolstoy/views.py
+++ leo_tolstoy/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    return render(request, 'index.html')
+
+def show_data(request):
+    return render(request, 'data.html')
+
+def start_search(request):
+    return render(request, 'search.html')
+
