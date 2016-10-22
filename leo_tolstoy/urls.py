e2d756c33a01abb82d3070bb24162cf10c1d44f5
alenush <alenusch93@yandex.ru>
2016-10-22 12:13:34 +0300

    Data page is ready!

--- leo_tolstoy/urls.py
+++ leo_tolstoy/urls.py
@@ -4,12 +4,20 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'data$', views.show_data, name='show_data'),
-    url(r'search$', views.start_search, name='search_main'),
+    url(r'data/$', views.show_data, name='show_data'),
+    url(r'search/$', views.start_search, name='search_main'),
 
+    url(r'data/volums/$', views.show_volums, name='volums'),
+    url(r'data/public/$', views.show_public, name='public'),
+    url(r'data/diaries/$', views.show_diaries, name='diaries'),
+    url(r'data/letters/$', views.show_letters, name='letters'),
+    url(r'data/children/$', views.show_children, name='children'),
+    url(r'data/church/$', views.show_church, name='church'),
+    url(r'data/chud/$', views.show_chudozh, name='chud'),
 
+    url(r'data/result/$', views.find_doc, name='find_doc'),
 
-    url(r'^data/download/(?P<tag>.+)/$', views.all_files_download, name='files_download'),
+    url(r'data/download/(?P<tag>.+)/$', views.all_files_download, name='files_download'),
 
     url(r'^searching/$', views.search_big, name='all_search'),
     url(r'^simple_search/$', views.simple_search, name='lemma_search'),
--- leo_tolstoy/views.py
+++ leo_tolstoy/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import os
 import zipfile
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+import codecs
 
 from leo_tolstoy.models import Works
 
@@ -12,12 +14,92 @@ def index(request):
     return render(request, 'index.html')
 
 def show_data(request):
-    print("Length: ", len(Works.objects.all()))
-    all_works = Works.objects.all()[1000:1200]
+    all_works = Works.objects.all()
+    paginator = Paginator(all_works, 100)
+    page = request.GET.get('page')
+    try:
+         my_works = paginator.page(page)
+    except PageNotAnInteger:
+        my_works = paginator.page(1)
+    except EmptyPage:
+        my_works = paginator.page(paginator.num_pages)
+    return render(request, 'data.html', {"works":my_works, 'volums':[]})
+
+def show_chudozh(request):
+    chudozh = set()
+    for works in Works.objects.filter(sphere='художественная'):
+        chudozh.add(works)
+    paginator = Paginator(list(chudozh), 60)
+    page = request.GET.get('page')
+    try:
+        chud_works = paginator.page(page)
+    except PageNotAnInteger:
+        chud_works = paginator.page(1)
+    except EmptyPage:
+        chud_works = paginator.page(paginator.num_pages)
+
+    return render(request, 'data.html', {"works": chud_works})
+
+def show_volums(request):
     volums = set()
     for works in Works.objects.all():
         volums.add(works.value)
-    return render(request, 'data.html', {"works":all_works, 'volums':volums})
+    return render(request, 'data.html', {"works": [], 'volums': volums})
+
+def show_public(request):
+    public = set()
+    for works in Works.objects.filter(sphere='публицистика'):
+        public.add(works)
+    paginator = Paginator(list(public), 60)
+    page = request.GET.get('page')
+    try:
+        public_works = paginator.page(page)
+    except PageNotAnInteger:
+        public_works = paginator.page(1)
+    except EmptyPage:
+        public_works = paginator.page(paginator.num_pages)
+    return render(request, 'data.html', {"works": public_works})
+
+def show_diaries(request):
+    diaries = set()
+    for works in Works.objects.filter(sphere='дневники и записные книжки'):
+        diaries.add(works)
+    return render(request, 'data.html', {"works": diaries})
+
+def show_letters(request):
+    letters = set()
+    for works in Works.objects.filter(sphere='Личная и деловая переписка'):
+        letters.add(works)
+    paginator = Paginator(list(letters), 100)
+    page = request.GET.get('page')
+    try:
+        letters_works = paginator.page(page)
+    except PageNotAnInteger:
+        letters_works = paginator.page(1)
+    except EmptyPage:
+        letters_works = paginator.page(paginator.num_pages)
+    return render(request, 'data.html', {"works": letters_works})
+
+def show_children(request):
+    children = set()
+    for works in Works.objects.filter(sphere='детская литература'):
+        children.add(works)
+    return render(request, 'data.html', {"works": children})
+
+def show_church(request):
+    church = set()
+    for works in Works.objects.filter(sphere='Церковно-богословская литература'):
+        church.add(works)
+    return render(request, 'data.html', {"works": church})
+
+def find_doc(request):
+    search_works = set()
+    if request.method == "POST":
+        query = request.POST.get('query')
+        print('REQUEST', query)
+        for works in Works.objects.filter(name__contains=query):
+            search_works.add(works)
+    return render(request, 'data.html', {"works": search_works})
 
 def start_search(request):
     vol_array = range(1,91)
@@ -33,27 +115,28 @@ def all_files_download(request, tag):
     :param tag: all, vol_n, file
     :return:
     """
-    print("Tag from template: ", tag)
+    # print("Tag from template: ", tag)
     dic_of_docs = {'all':'xml_data.zip'}
     for folder in os.listdir(os.path.realpath(os.getcwd())+'/leo_tolstoy/xml_data/'):
         if folder.endswith('zip'):
             vol = folder.split('.')[0].replace('_','')
             dic_of_docs[vol] = folder
+    global path_to_file
     if 'xhtml' in tag:
-        for filename, dirs, data_array in os.walk(os.getcwd()+'/leo_tolstoy/xml_data/'):
-            tag = str(tag).encode('utf8')
-            if tag in data_array:
-                new_tag = filename+'/'+tag
-                path_to_file = str(new_tag)
-                dic_of_docs[tag] = tag
+        for root, dirs, filenames in os.walk(os.getcwd()+'/leo_tolstoy/xml_data/'):
+            for filename in filenames:
+                if os.path.split(filename)[-1].decode('utf-8') == tag:
+                    path_to_file = os.path.join(root, filename)
     else:
-        if 'vol' in tag:
-            tag = tag.split('vol')[1]
-            print("VOL ", tag)
-        path_to_file = os.path.dirname(os.path.realpath(__file__)) + "/xml_data/"+dic_of_docs.get(str(tag))
+         if 'vol' in tag:
+             tag = tag.split('vol')[1]
+             path_to_file = os.path.dirname(os.path.realpath(__file__)) + "/xml_data/"+dic_of_docs.get(str(tag))
     my_file = open(path_to_file, 'r')
     response = HttpResponse(my_file, content_type='application/force-download')
-    response['Content-Disposition'] = 'attachment; filename=%s' % dic_of_docs[str(tag)]
+    if tag == 'all':
+        response['Content-Disposition'] = 'attachment; filename=%s' % 'all_xml.zip'
+    else:
+        response['Content-Disposition'] = 'attachment; filename=%s' % path_to_file.split('/')[-1]
     return response
 
 
